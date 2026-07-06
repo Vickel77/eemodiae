77d2cbe177fc578f993c74faccb790b0ce8f298d
Kelechi <kelechialigwo77@gmail.com>
2026-07-06 08:18:48 +0100

    dvc demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import NProgress from "nprogress";
 import "../styles/globals.css";
 import "../styles/dvc-viewer.css";
+import "../styles/dvc-experimental.css";
 import Router from "next/router";
 import AOS from "aos";
 import "aos/dist/aos.css";
